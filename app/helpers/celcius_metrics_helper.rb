1188e69d1d81c1197d8e0c92a2fde623811660e5
Jesse Emond <emond.jesse@gmail.com>
2015-09-01 03:33:48 -0400

    + weighted sum indicator helper and test

--- app/helpers/celcius_metrics_helper.rb
+++ app/helpers/celcius_metrics_helper.rb
@@ -2,4 +2,8 @@ module CelciusMetricsHelper
   def self.savard_degrees(temperatures)
     temperatures.sum.to_f / temperatures.size
   end
+
+  def self.weighted_degrees_indicator(temperatures)
+    temperatures.sort[1..-2].sum
+  end
 end
--- test/helpers/celcius_metrics_helper_test.rb
+++ test/helpers/celcius_metrics_helper_test.rb
@@ -4,4 +4,8 @@ class CelciusMetricsHelperTest < ActiveSupport::TestCase
   test 'should return average for savard degrees' do
     assert_equal 27, CelciusMetricsHelper.savard_degrees([4, 0, 4, 100])
   end
+
+  test 'should return correct sum for weighted degrees indicator' do
+    assert_equal 301, CelciusMetricsHelper.weighted_degrees_indicator([0,-500,10000,300,1])
+  end
 end
