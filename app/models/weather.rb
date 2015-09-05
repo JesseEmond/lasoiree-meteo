94b4ab3d79bda037255b26b1e59926e6cdec6271
Jesse Emond <emond.jesse@gmail.com>
2015-09-04 20:59:05 -0400

    Use OpenWeather for the weather data

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,10 +1,24 @@
+require 'open_weather'
+
 class Weather
   def canadian_cities_weather
-    (1..24).map do |i|
+    options = { units: 'metric' }
+    options = options.merge({ APPID: ENV['APPID'] }) unless ENV['APPID'].nil?
+
+    weather = OpenWeather::Current.cities(CANADIAN_CITY_IDS, options)
+    weather['list'].map do |city_weather|
       {
-        name: 'Lorem ' + i.to_s,
-        celcius: -10 + Random.rand(20),
+        name: city_weather['name'],
+        celcius: city_weather['main']['temp']
       }
     end
   end
+
+  private
+  # major cities only, taken from http://www.meteomedia.com/, using
+  # http://openweathermap.org/ to find IDs.
+  CANADIAN_CITY_IDS = [5909628,5913490,5920288,5946768,5959974,6324729,
+                       5983720,6050610,6059891,6076211,6077243,6094817,
+                       6325494,6141256,6146143,6138517,6138501,6163012,
+                       6167865,6169141,6173331,6173331,6183235,6185377]
 end
