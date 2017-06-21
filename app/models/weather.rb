8b0ee6adb45f26eba0be44020c525564e40d55e9
Jesse Emond <emond.jesse@gmail.com>
2017-06-21 08:30:39 -0400

    Quick fix for the 20 cities limitation
    Will update the API repo

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -6,6 +6,8 @@ class Weather
     options = options.merge({ APPID: ENV['APPID'] }) unless ENV['APPID'].nil?
 
     weather = OpenWeather::Current.cities(CANADIAN_CITY_IDS, options)
+
+    puts weather
     
     weather['list'].map do |city_weather|
       {
@@ -18,8 +20,8 @@ class Weather
   private
   # major cities only, taken from http://www.meteomedia.com/, using
   # http://openweathermap.org/ to find IDs.
-  CANADIAN_CITY_IDS = [5909628,5913490,5920288,5946768,5959974,6324729,
-                       5983720,6050610,6059891,6076211,6077243,6094817,
-                       6325494,6141256,6146143,6138517,6138501,6163012,
-                       6167865,6169141,6173331,6180550,6183235,6185377]
+  CANADIAN_CITY_IDS = [5909628,5913490,5920288,5946768,5959974,
+                       5983720,6050610,6059891,6076211,6077243,
+                       6325494,6141256,6146143,6138517,6138501,
+                       6167865,6169141,6173331,6180550,6183235]
 end
